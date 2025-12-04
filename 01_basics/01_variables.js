21cd3345856f4e97635f0d1ad8dbaa02d133a083
A4RIN <aarinbadola633@gmail.com>
2025-12-04 20:14:28 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144453
+//accountId = 2 this would not edit the value assigned through const keyword.
+console.log(accountId);
+let accountEmail= "aarin@xyz.com"
+var accountPassword= "9876"
+
+accountEmail= "badola@abc.com"
+accountPassword= "1234"
+console.table([accountId, accountEmail, accountPassword])
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+let name = "aarin"
+let age= 21
+let agex= "21"
+let isLoggedIn = false
+let state;
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof agex)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof null)
