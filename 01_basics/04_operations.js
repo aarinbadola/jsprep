c3af3e200dfe51da23020000c00af64267923196
A4RIN <aarinbadola633@gmail.com>
2025-12-05 13:08:02 +0530

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,23 @@
+let valeue = 3
+let negValue= -valeue
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = "aarin"
+let str3 = str1+str2
+console.log(str3)
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1"+"2")
+console.log(1+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
